e9f0ce8e2b291d8d105adc69fa8a380bce964065
Ivan Alvarado <ivan.alvarado1090@gmail.com>
2020-09-16 18:18:51 -0500

    docs agregado

---
+++ docs/precache-manifest.bdb39d458f37dfcb6ec9bc2b32c37e84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065765cad45c89c445ad44781f5050dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76cb11168f5f9d0924a4",
+    "url": "/static/css/main.287efc74.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76cb11168f5f9d0924a4",
+    "url": "/static/js/main.914b105a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
